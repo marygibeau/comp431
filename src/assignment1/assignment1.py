b8108db3f0c9cb27a981804951105053e5145169
unknown <marygold1011@gmail.com>
2018-08-24 18:24:13 -0400

    first commit

---
+++ src/assignment1/assignment1.py
@@ -0,0 +1,12 @@
+# for loop of sys.stdin to read in char by char
+# remember to spit out where the error occured (ex. mailbox, name, path)
+
+# for stdin
+# is mail valid if no print error and end process
+# is from valid if no print error and end process
+# etc until pass all processes
+# print send Ok and end process
+
+x = 'a' == 'a'
+
+print(x)
